8f916548baba70b36af3ce77345f4a5fa0ea613d
Travis <tracisz@hotmail.com>
2024-03-08 20:30:35 -0500

    back up

---
+++ non-nix/waybar/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A basic flake for waybar nvim dev";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+        devShells.${system}.default = 
+          pkgs.mkShell
+            {
+                buildInputs = with pkgs; [
+                  neovim
+                  vim
+                ];
+
+                shellHook = ''
+                  echo "hello mom"
+                  '';
+              };
+  };
+}
